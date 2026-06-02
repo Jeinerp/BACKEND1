82328176bcd0745d9acabdc84ebc5378a974ff51
Jeinerp <143730457+Jeinerp@users.noreply.github.com>
2026-06-02 15:35:42 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['*'] # En producción puedes ser más específico
 CSRF_TRUSTED_ORIGINS = [
    'https://backend1-production-75db.up.railway.app', # Sin el guion medio
-    'https://monitoreoiot.netlify.app/'
+    'https://monitoreoiot.netlify.app'
 ]
 
 # Application definition
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Agrega esta línea
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://monitoreoiot.netlify.app/",
+    "https://monitoreoiot.netlify.app",
     "backend1-production-75db.up.railway.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
