6d8fb945ccf9561e9b0917df5a9120ce00648f5c
Jeinerp <143730457+Jeinerp@users.noreply.github.com>
2026-06-03 14:41:47 -0500

    e

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 # Permitir el dominio de Railway y localhost
 ALLOWED_HOSTS = ['*'] # En producción puedes ser más específico
 CSRF_TRUSTED_ORIGINS = [
+    'https://backend1-production-75db.up.railway.app',
+    'https://monitoreoiot.netlify.app',
     'https://playaiot-production.up.railway.app', # Sin el guion medio
     'https://confident-insight-production.up.railway.app'
 ]
@@ -132,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Agrega esta línea
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
+    "https://monitoreoiot.netlify.app",
     "https://confident-insight-production.up.railway.app",
     "https://playaiot-production.up.railway.app"
 ]
